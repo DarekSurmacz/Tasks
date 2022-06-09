fbef0e2508ace0cf84443e20c1c60c2aef840b96
Dariusz Surmacz <dariuszsurmacz6@gmail.com>
2022-06-09 16:33:12 +0200

    19.2

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,8 +1,26 @@
 package com.crud.tasks.domain;
 
-public class Task {
-    private Long id;
-    private String title;
-    private String content;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name")
+    private String title;
+
+    @Column(name = "description")
+    private String content;
 }
