b0d9136c31528ae58b688f37c14ade35aef1e090
Dariusz Surmacz <dariuszsurmacz6@gmail.com>
2022-06-16 20:54:15 +0200

    19.2 poprawione

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -14,13 +14,4 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     //nadpisujemy metodę, oryginalnie zwraca typ Iterable<T> , wolimy List<Task>
     List<Task> findAll();
-
-    @Override
-    Task save(Task task);
-
-    @Override
-    Optional<Task> findById(Long id);
-
-    @Override
-    void deleteById(Long id);
 }
