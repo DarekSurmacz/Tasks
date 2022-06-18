3bc386b64b2c38a555c188ec218a34dbcfc6f18a
Dariusz Surmacz <dariuszsurmacz6@gmail.com>
2022-06-18 14:40:31 +0200

    20.4

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/tasks")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class TaskController {
 //   Controller jest częścią aplikacji webowej, która odpowiada za otrzymywanie żądań i zwracanie odpowiedzi.
 //   Wewnątrz controllera wywoływane są metody klas implementujących logikę biznesową.
