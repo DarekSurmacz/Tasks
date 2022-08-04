4b8d547af62bae38396287511939806758790e7c
Dariusz Surmacz <dariuszsurmacz6@gmail.com>
2022-08-04 16:36:43 +0200

    22.3

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -20,17 +20,20 @@ public class TrelloController {
 
     @GetMapping("boards")
     public void getTrelloBoards() {
-
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-//        trelloBoards.forEach(trelloBoardDto -> {
-//            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-//        });
+        trelloBoards.forEach(trelloBoardDto -> {
+            System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
+            System.out.println("This board contains lists: ");
+            trelloBoardDto.getLists().forEach(trelloList -> {
+                System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
+            });
+        });
 
-        trelloBoards.stream()
-                .filter(trelloBoardDto -> trelloBoardDto.getId() != null)
-                .filter(trelloBoardDto -> trelloBoardDto.getName() != null)
-                .filter(trelloBoardDto -> trelloBoardDto.getName().contains(("Kodilla")))
-                .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + "" + trelloBoardDto.getName()));
+//        trelloBoards.stream()
+//                .filter(trelloBoardDto -> trelloBoardDto.getId() != null)
+//                .filter(trelloBoardDto -> trelloBoardDto.getName() != null)
+//                .filter(trelloBoardDto -> trelloBoardDto.getName().contains(("Kodilla")))
+//                .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + "" + trelloBoardDto.getName()));
     }
 }
--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -25,7 +25,7 @@ public class TrelloClient {
     private String trelloAppKey;
     @Value("${trello.app.token}")
     private String trelloToken;
-    @Value("$[trello.app.username}")
+    @Value("${trello.app.username}")
     private String trelloUsername;
 
     //    UriComponentsBuilder - klasa pozwalająca na tworzenie adresu URL
@@ -39,15 +39,15 @@ public class TrelloClient {
     }
 
     private URI makeURL() {
-        URI url = UriComponentsBuilder
+
+        return UriComponentsBuilder
                 .fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUsername + "/boards")
                 .queryParam("key", trelloAppKey)
                 .queryParam("token", trelloToken)
                 .queryParam("fields", "name,id")
+                .queryParam("lists", "all")
                 .build() //buduje obiekt UriComponents
                 .encode() //koduje czyli transformuje adres URL
-                .toUri(); //transformuje obiekt UriComponents na obiekt URI
-
-        return url;
+                .toUri();
     }
 }
