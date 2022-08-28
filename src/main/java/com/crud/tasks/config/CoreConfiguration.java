81bb4ec20e0b5049abc3b59eaebe753495789ab2
Dariusz Surmacz <dariuszsurmacz6@gmail.com>
2022-08-28 19:14:57 +0200

    23.4

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -1,14 +1,16 @@
 package com.crud.tasks.config;
 
-        import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-        import org.springframework.context.annotation.Bean;
-        import org.springframework.context.annotation.Configuration;
-        import org.springframework.web.client.RestTemplate;
-        import springfox.documentation.builders.PathSelectors;
-        import springfox.documentation.builders.RequestHandlerSelectors;
-        import springfox.documentation.spi.DocumentationType;
-        import springfox.documentation.spring.web.plugins.Docket;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
+@EnableScheduling
 //@EnableSwagger2
 @OpenAPIDefinition
 @Configuration //utworzenie miejsca dla tworzenia "ręcznych" beanów
